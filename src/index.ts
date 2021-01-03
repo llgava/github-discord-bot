b622e9cafda5aeee1a59b8d0eacdb4a68d5b8178
Leonardo Luiz Gava <llgavamt@gmail.com>
2021-01-03 08:01:53 -0400

    Add Octokit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 require("dotenv").config();
 import Discord from 'discord.js';
 import WOKCommands from 'wokcommands';
+import { Octokit } from "@octokit/core";
 
 const Client = new Discord.Client();
+const GitHub = new Octokit({auth: process.env.GITHUB_TOKEN})
 
-Client.on("ready", () => {
+Client.on("ready", async () => {
   new WOKCommands(Client, 'commands');
-  
+
+  const res = await GitHub.request("GET /users/{username}/repos", {
+    username: "llgava",
+  });
+
   console.clear();
-  console.log('O bot está online.');
+  console.log(res.data.find(repo => repo.full_name === 'llgava/llgava'));
 });
 
 Client.login(process.env.BOT_TOKEN);
